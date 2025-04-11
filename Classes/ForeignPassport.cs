36780b8611e6a4d7078751e080b87a30c3dde31c
Petre_to82@student.itstep.org <Petre_to82@student.itstep.org>
2025-04-11 16:48:59 +0300

    ex3

---
+++ Classes/ForeignPassport.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp7.Classes
+{
+    public class ForeignPassport
+    {
+        private string _passportNumber;
+        private string _fullName;
+        private DateTime _issueDate;
+        private DateTime _expiryDate;
+        private string _issuingAuthority;
+
+        public ForeignPassport(string passportNumber, string fullName, DateTime issueDate,
+                             DateTime expiryDate, string issuingAuthority)
+        {
+            PassportNumber = passportNumber;
+            FullName = fullName;
+            IssueDate = issueDate;
+            ExpiryDate = expiryDate;
+            IssuingAuthority = issuingAuthority;
+        }
+
+        public string PassportNumber
+        {
+            get => _passportNumber;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Номер паспорта не може бути порожнім");
+
+                if (!Regex.IsMatch(value, @"^[A-Z0-9]{6,9}$"))
+                    throw new ArgumentException("Номер паспорта має містити 6-9 літер або цифр");
+
+                _passportNumber = value;
+            }
+        }
+
+        public string FullName
+        {
+            get => _fullName;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("ПІБ не може бути порожнім");
+
+                if (value.Length < 5 || value.Length > 100)
+                    throw new ArgumentException("ПІБ має містити від 5 до 100 символів");
+
+                if (!Regex.IsMatch(value, @"^[\p{L}\s'-]+$"))
+                    throw new ArgumentException("ПІБ має містити лише літери, пробіли, апостроф або дефіс");
+
+                _fullName = value;
+            }
+        }
+
+        public DateTime IssueDate
+        {
+            get => _issueDate;
+            set
+            {
+                if (value > DateTime.Now)
+                    throw new ArgumentException("Дата видачі не може бути у майбутньому");
+
+                if (value < new DateTime(1991, 1, 1)) 
+                    throw new ArgumentException("Дата видачі не може бути раніше 1991 року");
+
+                _issueDate = value;
+            }
+        }
+
+        public DateTime ExpiryDate
+        {
+            get => _expiryDate;
+            set
+            {
+                if (value <= IssueDate)
+                    throw new ArgumentException("Дата закінчення дії має бути після дати видачі");
+
+                if (value < DateTime.Now)
+                    throw new ArgumentException("Паспорт вже протермінований");
+
+                _expiryDate = value;
+            }
+        }
+
+        public string IssuingAuthority
+        {
+            get => _issuingAuthority;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Орган видачі не може бути порожнім");
+
+                if (value.Length < 3 || value.Length > 50)
+                    throw new ArgumentException("Назва органу видачі має містити від 3 до 50 символів");
+
+                _issuingAuthority = value;
+            }
+        }
+
+        public bool IsValid()
+        {
+            return DateTime.Now < ExpiryDate && DateTime.Now > IssueDate;
+        }
+
+        public override string ToString()
+        {
+            return $"Паспорт №{PassportNumber}\n" +
+                   $"Власник: {FullName}\n" +
+                   $"Виданий: {IssueDate:dd.MM.yyyy}\n" +
+                   $"Дійсний до: {ExpiryDate:dd.MM.yyyy}\n" +
+                   $"Орган видачі: {IssuingAuthority}\n" +
+                   $"Статус: {(IsValid() ? "Дійсний" : "Не дійсний")}";
+        }
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,10 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.OutputEncoding = System.Text.Encoding.UTF8;
-            Console.InputEncoding = System.Text.Encoding.UTF8;
+            try
+            {
+                var passport1 = new ForeignPassport(
+                    "AB123456",
+                    "Іванов Іван Іванович",
+                    new DateTime(2018, 5, 15),
+                    new DateTime(2028, 5, 14),
+                    "ДМСУ м. Києва");
 
-            Application.Run();
+                Console.WriteLine("Успішно створений паспорт:");
+                Console.WriteLine(passport1);
+                Console.WriteLine();
+
+                try
+                {
+                    var passport2 = new ForeignPassport(
+                        "123", 
+                        "Ів", 
+                        new DateTime(2030, 1, 1), 
+                        new DateTime(2020, 1, 1), 
+                        ""); 
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine("Помилка при створенні паспорта:");
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Сталася неочікувана помилка: {ex.Message}");
+            }
         }
     }
 }
