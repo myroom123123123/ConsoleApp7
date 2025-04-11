4c4b4db0e594427a4d88ac39eb2a7f3facf1137b
Petre_to82@student.itstep.org <Petre_to82@student.itstep.org>
2025-04-11 16:38:26 +0300

    ex2

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -7,41 +7,7 @@
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             Console.InputEncoding = System.Text.Encoding.UTF8;
 
-            while (true)
-            {
-                Menu.ShowMainMenu();
-                string choice = Console.ReadLine();
-
-                switch (choice)
-                {
-                    case "1":
-                        ConversionHandler.HandleConversion(10, 2, "Десяткова", "Двійкова");
-                        break;
-                    case "2":
-                        ConversionHandler.HandleConversion(2, 10, "Двійкова", "Десяткова");
-                        break;
-                    case "3":
-                        ConversionHandler.HandleConversion(10, 8, "Десяткова", "Вісімкова");
-                        break;
-                    case "4":
-                        ConversionHandler.HandleConversion(8, 10, "Вісімкова", "Десяткова");
-                        break;
-                    case "5":
-                        ConversionHandler.HandleConversion(10, 16, "Десяткова", "Шістнадцяткова");
-                        break;
-                    case "6":
-                        ConversionHandler.HandleConversion(16, 10, "Шістнадцяткова", "Десяткова");
-                        break;
-                    case "7":
-                        return;
-                    default:
-                        Console.WriteLine("\nНевірний вибір!");
-                        break;
-                }
-
-                Console.WriteLine("\nНатисніть будь-яку клавішу для продовження...");
-                Console.ReadKey();
-            }
+            Application.Run();
         }
     }
 }
---
+++ Classes/WordToNumberConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp7.Classes
+{
+    public static class WordToNumberConverter
+    {
+        private static readonly Dictionary<string, int> wordToNumberMap = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase)
+        {
+            {"zero", 0},
+            {"one", 1},
+            {"two", 2},
+            {"three", 3},
+            {"four", 4},
+            {"five", 5},
+            {"six", 6},
+            {"seven", 7},
+            {"eight", 8},
+            {"nine", 9}
+        };
+
+        public static int Convert(string word)
+        {
+            if (wordToNumberMap.TryGetValue(word, out int number))
+            {
+                return number;
+            }
+            throw new ArgumentException($"Невідоме слово: {word}");
+        }
+
+        public static bool IsValidWord(string word)
+        {
+            return wordToNumberMap.ContainsKey(word.ToLower());
+        }
+    }
+}
